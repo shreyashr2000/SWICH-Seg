b8a45b8875f7d62e03a75c929c557f4ed5a93cd0
shreyashr2000 <114232340+shreyashr2000@users.noreply.github.com>
2024-04-24 01:16:07 +0530

    Update data_preprocessing_training.py

--- data_preprocessing_training.py
+++ data_preprocessing_training.py
@@ -1,7 +1,7 @@
 import nibabel as nib
 import numpy as np
 
-def load_ins_data(data, mask):
+def load_train_data(data, mask):
     """
     Load CT scan data and convert corresponding masks to one-hot encoding from the INSTANCE Challenge dataset.
 
@@ -45,6 +45,39 @@ def load_ins_data(data, mask):
         for j in range(32):
             if j < data[i].shape[2]:
                 ins_data[i * 32 + j, :, :] = np.rot90(img_data[i][:, :, j])
+                mask = np.rot90(mask_data[i][:, :, j])
+                ins_mask[i * 32 + j, 0, :, :] = 1 - mask  # Background
+                ins_mask[i * 32 + j, 1, :, :] = mask  # Target
+            else:
+                ins_data[i * 32 + j, :, :] = np.rot90(img_data[i][:, :, j-1])
+                mask = np.rot90(mask_data[i][:, :, j-1])
+                ins_mask[i * 32 + j, 0, :, :] = 1 - mask  # Background
+                ins_mask[i * 32 + j, 1, :, :] = mask  # Target
+                
+            # Set binary label based on the presence of target in the mask
+            if ins_mask[i * 32 + j, 1, :, :].max() > 0:
+                ins_label[i * 32 + j] = 1
+                
+    return ins_data, ins_mask, ins_label
+def load_test_data(data, mask):
+    img_data = []  # List to store loaded CT scan data
+    mask_data = []  # List to store loaded masks
+    ins_data = np.zeros((len(data) * 32, 512, 512))  # Initialize array for CT scan data
+    ins_mask = np.zeros((len(data) * 32, 2, 512, 512))  # Initialize array for masks
+    ins_label = np.zeros(len(data) * 32)  # Initialize array for binary labels
+    
+    # Load CT scan data and masks
+    for i in range(len(data)):
+        nifti_data = nib.load(data[i])
+        img_data.append(nifti_data.get_fdata())
+        
+        nifti_mask = nib.load(mask[i])
+        z=nifti_mask.get_fdata()
+        z[z>0]=1
+        mask_data.append(z)
+    # Process CT scan data and masks
+    for i in range(len(data)):
+                ins_data[i * 32 + j, :, :] = np.rot90(img_data[i][:, :, :])
                 mask = np.rot90(mask_data[i][:, :, j])
                 ins_mask[i * 32 + j, 0, :, :] = 1 - mask  # Background
                 ins_mask[i * 32 + j, 1, :, :] = mask  # Target
