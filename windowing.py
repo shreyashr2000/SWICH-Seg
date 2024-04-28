0a1775ef52e93f79a277ab2c7a59713dd841675e
shreyashr2000 <114232340+shreyashr2000@users.noreply.github.com>
2024-04-29 02:40:28 +0530

    Update windowing.py

--- windowing.py
+++ windowing.py
@@ -1,14 +1,15 @@
+# For RSNA Dataset
 def window_image(img, window_center=30, window_width=80, intercept=-1024.0, slope=1.0):
     img = img * slope + intercept
     img_min = window_center - window_width // 2
     img_max = window_center + window_width // 2
     slice_s=img
-#    print(slice_s.shape)
     slice_s = (slice_s - img_min)*(255/(img_max-img_min)) #or slice_s = (slice_s - (w_level-(w_width/2)))*(255/(w_width))
     slice_s[slice_s < 0]=0
     slice_s[slice_s > 255] = 255
 
     return slice_s
+#For INSTANCE Dataset
 def window_ct (ct_scan, w_level=35, w_width=80):
     w_min = w_level - w_width / 2
     w_max = w_level + w_width / 2
