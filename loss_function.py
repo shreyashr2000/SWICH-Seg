b63ac3ae46dcb91397c4c8f0dab770a00e4dc183
shreyashr2000 <114232340+shreyashr2000@users.noreply.github.com>
2024-04-29 02:41:53 +0530

    Update loss_function.py

--- loss_function.py
+++ loss_function.py
@@ -6,12 +6,8 @@ class dice_loss(nn.Module):
     def forward(self, x, target):
         num_classes = target.shape[1]   # Channels first
         target = target.type(x.type())
-        dims =  tuple(range(2, target.ndimension()))
+        dims = (0,) + tuple(range(2, target.ndimension()))
         intersection = torch.sum(x * target, dims)
         cardinality = torch.sum(x + target, dims)
-        if torch.sum(intersection) + torch.sum(cardinality) == 0:
-            return 0.0
-        if torch.sum(intersection)==0:
-            return 1
         dice_loss = (2. * intersection / (cardinality + self.eps)).mean()
         return (1 - dice_loss)
