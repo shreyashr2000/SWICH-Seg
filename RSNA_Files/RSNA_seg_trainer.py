5e1c13c41a7968ed2d048d178f9c600c6e7c06a3
shreyashr2000 <114232340+shreyashr2000@users.noreply.github.com>
2024-05-02 01:05:37 +0530

    Add files via upload

---
+++ RSNA_Files/RSNA_seg_trainer.py
@@ -0,0 +1,80 @@
+from augmentation import augment1
+from augmentation import augment2
+from windowing import window_image
+def seg_train_model_lowdata(model, train_loader, val_loader, criterion, optimizer, device, num_epochs, save_path):
+    best_val_loss = float('inf')
+    best_model = None
+
+    for epoch in range(num_epochs):
+        # Training phase
+        model.train()
+        running_train_loss = 0.0
+
+        for inputs, targets,wc,ww,inter,slp in tqdm(train_loader, desc=f'Training Epoch {epoch + 1}/{num_epochs}'):
+            one_d_data = torch.zeros((inputs.shape[0], 1, 512, 512)).to(device)
+            for i in range(inputs.shape[0]):
+                data1 = augment1(inputs[i,:,:])
+                if isinstance(data1, np.ndarray):
+                    gray_img = torch.from_numpy(data1)
+                else:
+                    gray_img = data1
+
+                r = window_image(data[i,:,:],  window_center=wc[i], window_width=ww[i], intercept=inter[i], slope=slp[i])  # Extract R channel
+                # Normalize the channel
+                r = r / r.max()
+                # Scale each channel to [0, 255] range
+                r = (r * 255).clamp(0, 255)
+                # Assign R, G, B channels to respective positions in the new tensor
+                if not torch.isinf(r).any().item():
+                    one_d_data[i, 0, :, :] = r
+            inputs, targets = one_d_data.to(device), targets.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+            running_train_loss += loss.item() * inputs.size(0)
+
+        train_loss = running_train_loss / len(train_loader.dataset)
+
+        # Validation phase
+        model.eval()
+        running_val_loss = 0.0
+
+        with torch.no_grad():
+            for inputs, targets,wc,ww,inter,slp in tqdm(val_loader, desc=f'Validation Epoch {epoch + 1}/{num_epochs}'):
+                one_d_data = torch.zeros((inputs.shape[0], 1, 512, 512)).to(device)
+                for i in range(inputs.shape[0]):
+                    gray_img = inputs[i,:,:]
+                    r = window_image(data[i,:,:],  window_center=wc[i], window_width=ww[i], intercept=inter[i], slope=slp[i])  # Extract R channel
+     
+
+                    # Normalize each channel individually
+                    r = r / r.max()
+   
+
+                    # Scale each channel to [0, 255] range
+                    r = (r * 255).clamp(0, 255)
+ 
+                    # Assign R, G, B channels to respective positions in the new tensor
+                    if not torch.isinf(r).any().item():
+                        one_d_data[i, 0, :, :] = r
+    
+
+                inputs, targets = one_d_data.to(device), targets.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, targets)
+                running_val_loss += loss.item() * inputs.size(0)
+
+        val_loss = running_val_loss / len(val_loader.dataset)
+
+        # Save the best model based on validation loss
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            best_model = model.state_dict()
+            torch.save(best_model, save_path)
+
+        print(f'Epoch {epoch + 1}/{num_epochs}, Training Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}')
+
+    print('Training complete!')
+    return best_model
